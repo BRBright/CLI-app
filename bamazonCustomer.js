065aeb96bfe6213e7ff0c24aae50ce674a468e55
Billy Bright <billybright1992@gmail.com>
2019-09-04 16:05:17 -0700

    app put into loop until suser inputs end, end module still needs to be written

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,42 +18,71 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   //console.log("connected as id " + connection.threadId + "\n");
+  startEnd();
 });
-
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "userItem",
-      message: "What is the ID number of the item you are looking for?"
-    },
-    {
-      type: "input",
-      name: "userQuantity",
-      message: "How many would you like?"
-    }
-  ])
-  .then(function(user) {
-    console.log("Selecting product...\n");
-    let itemQuery = user.userItem;
-    connection.query("SELECT * FROM products WHERE id=?", [itemQuery], function(
-      err,
-      res
-    ) {
-      if (err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log(
-          res[i].id +
-            ") " +
-            res[i].product_name +
-            " | " +
-            res[i].department_name +
-            " | " +
-            res[i].price +
-            " | " +
-            res[i].stock_quantity
-        );
+function startEnd() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "search_end",
+        message: "Would you like to search for a product?",
+        choices: ["Search", "END"]
       }
-      connection.end();
+    ])
+    .then(function(answer) {
+      if (answer.search_end === "Search") {
+        runSearch();
+      } else if (answer.search_end === "END") {
+        connection.end();
+      }
     });
-  });
+}
+
+function runSearch() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "userItem",
+        message: "What is the ID number of the item you are looking for?"
+      },
+      {
+        type: "input",
+        name: "userQuantity",
+        message: "How many would you like?"
+      }
+    ])
+    .then(function(user) {
+      console.log("Selecting product...\n");
+      let itemQuery = user.userItem;
+      let itemQuantity = user.userQuantity;
+      connection.query(
+        "SELECT * FROM products WHERE id=?",
+        [itemQuery],
+        function(err, res) {
+          if (err) throw err;
+
+          for (var i = 0; i < res.length; i++) {
+            if (itemQuantity <= res[i].stock_quantity) {
+              console.log(
+                res[i].id +
+                  ") " +
+                  res[i].product_name +
+                  " | " +
+                  res[i].department_name +
+                  " | " +
+                  res[i].price +
+                  " | " +
+                  res[i].stock_quantity
+              );
+            } else {
+              console.log("Insufficient quantity!");
+            }
+          }
+          startEnd();
+          connection.end();
+        }
+      );
+    });
+}
